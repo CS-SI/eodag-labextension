4f7754b27dda5611d01f28aa9e63ae2944731b1a
Christophe Triquet <christophe.triquet@c-s.fr>
2019-03-18 16:30:47 +0100

    eodag service home page

--- eodag_labextension/__init__.py
+++ eodag_labextension/__init__.py
@@ -5,6 +5,9 @@
 
 from notebook.base.handlers import IPythonHandler
 from notebook.utils import url_path_join
+from tornado import web
+
+from eodag.rest.service import get_home_page_content, get_templates_path
 
 __version__ = "0.1.0"
 
@@ -13,12 +16,20 @@ def _jupyter_server_extension_paths():
     return [{"module": "eodag_labextension"}]
 
 
-class HelloWorldHandler(IPythonHandler):
+class EodagHandler(IPythonHandler):
     """HelloWorld test class"""
 
-    def get(self):
+    @web.authenticated
+    def get(self, cluster_id: str = "") -> None:
         """A get test method"""
-        self.finish("Hello, world!")
+
+        # Update templates_path for Jinja FileSystemLoader
+        jinja_env = self.settings["jinja2_env"]
+        for loader in jinja_env.loader.loaders:
+            if hasattr(loader, "searchpath"):
+                loader.searchpath.append(get_templates_path())
+
+        self.write(self.render_template("index.html", content=get_home_page_content()))
 
 
 def load_jupyter_server_extension(nb_server_app):
@@ -35,8 +46,7 @@ def load_jupyter_server_extension(nb_server_app):
     nb_server_app.log.info(log)
     nb_server_app.log.info("#" * len(log))
 
-    # XXX dummy service
     web_app = nb_server_app.web_app
     host_pattern = ".*$"
-    route_pattern = url_path_join(web_app.settings["base_url"], "/hello")
-    web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
+    route_pattern = url_path_join(web_app.settings["base_url"], "/eodag")
+    web_app.add_handlers(host_pattern, [(route_pattern, EodagHandler)])
