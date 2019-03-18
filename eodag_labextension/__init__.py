243d4ac2f80054ad11e3280bdbfc5ca7dfb7d9d6
Christophe Triquet <christophe.triquet@c-s.fr>
2019-03-18 10:43:42 +0100

    Enhance logs

--- eodag_labextension/__init__.py
+++ eodag_labextension/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015-2017 CS Systemes d'Information (CS SI)
 # All rights reserved
+"""Jupyter Notebook server extension for eodag REST service"""
 
-from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
-
+from notebook.utils import url_path_join
 
 __version__ = "0.1.0"
 
@@ -14,7 +14,10 @@ def _jupyter_server_extension_paths():
 
 
 class HelloWorldHandler(IPythonHandler):
+    """HelloWorld test class"""
+
     def get(self):
+        """A get test method"""
         self.finish("Hello, world!")
 
 
@@ -26,9 +29,10 @@ def load_jupyter_server_extension(nb_server_app):
         nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
     """
 
-    # XXX Printed into JupyterHub console logs
-    nb_server_app.log.info("my module enabled!")
-    nb_server_app.log.info(100 * "#")
+    # Printed into JupyterHub console logs
+    nb_server_app.log.info("#" * 25)
+    nb_server_app.log.info(f"{__package__} enabled!")
+    nb_server_app.log.info("#" * 25)
 
     # XXX dummy service
     web_app = nb_server_app.web_app
