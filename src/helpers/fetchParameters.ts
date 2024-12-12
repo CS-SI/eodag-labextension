d772b8971a41e4eb6c1f76435ed18bf30e2d4855
anthony deplanque <anthony.deplanque@csgroup.eu>
2024-12-12 15:55:35 +0100

    functionnal parameter selector

--- src/helpers/fetchParameters.ts
+++ src/helpers/fetchParameters.ts
@@ -1,44 +1,60 @@
-export const fetchRawData = async (): Promise<any[]> => {
-  const filepaths = [
-    '/files/temp/DT_CLIMATE_ADAPTATION.json',
-    '/files/temp/ERA5_SL.json',
-    '/files/temp/S2_MSI_L1C.json'
-  ];
+export const fetchRawData = async (
+  provider: string,
+  productType: any
+): Promise<any[]> => {
+  console.log(provider);
+  console.log(productType);
+
+  const filepath = '/files/temp/AGGREGATE.json';
+  // TODO: - Edit lines below to adapt to API
 
   try {
-    const contents = await Promise.all(
-      filepaths.map(async filepath => {
-        const response = await fetch(filepath);
-        if (!response.ok) {
-          throw new Error(
-            `Erreur : impossible de trouver le fichier ${filepath}`
-          );
-        }
-        return await response.json();
-      })
-    );
-
-    return contents;
+    const response = await fetch(filepath);
+    if (!response.ok) {
+      throw new Error(`Erreur : impossible de trouver le fichier ${filepath}`);
+    }
+    return await response.json();
   } catch (error) {
     console.error('Erreur lors de la récupération des fichiers JSON :', error);
     throw error;
   }
 };
 
-export const determineIfMandatory = (
+export const determineMandatoryProperties = (
   data: any
-): [string, any, { mandatory: boolean }][] => {
+): { key: string; value: unknown; mandatory: boolean }[] => {
   const { properties, required } = data;
 
   if (!properties) {
     throw new Error("Erreur : 'properties' est manquant dans le schéma.");
   }
 
-  const result = Object.entries(properties).map(([key, value]) => {
-    const mandatory = required ? required.includes(key) : false;
-    return [key, value, { mandatory }] as [string, any, { mandatory: boolean }];
-  });
+  const uselessKeyToWithdraw = [
+    'start_datetime',
+    'end_datetime',
+    'year',
+    'month',
+    'day',
+    'time',
+    'datetime',
+    'geometry',
+    'bbox'
+  ];
+
+  const result = Object.entries(properties)
+    .map(([key, value]) => {
+      if (uselessKeyToWithdraw.includes(key)) return null; // remove useless key items
+
+      const mandatory = required ? required.includes(key) : false;
+      return { key, value, mandatory };
+    })
+    .filter(value => value !== null)
+    .sort((a, b) => {
+      return a.mandatory === b.mandatory ? 0 : a.mandatory ? -1 : 1;
+    }); // Sort by mandatory items
   return result;
 };
 
-// remove Geometry / start_datetime / end_datetime
+export const isAdditionalParameters = (data: any): boolean => {
+  return data?.additionalProperties ? data.additionalProperties : false;
+};
