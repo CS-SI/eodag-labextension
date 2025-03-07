e6c0d4c8fe547894269cd8a042e0562be8932c00
LAMBARE Aubin <aubin.lambare@csgroup.eu>
2025-03-07 18:43:57 +0100

    feat: rename 'additional parameters' to 'custom parameters'
    This is to clarify the purpose of this form section

--- src/formComponent/AdditionalParameterFields.tsx
+++ src/formComponent/AdditionalParameterFields.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useFieldArray, UseFormReturn } from 'react-hook-form';
 import { Tooltip } from 'react-tooltip';
-import { CarbonAddFilled, CarbonInformation, CarbonTrashCan } from '../icones';
+import { CarbonAddFilled, CarbonTrashCan } from '../icones';
 import { IFormInput } from '../types';
 import { tooltipDark, tooltipTop, tooltipWarning } from './FormComponent';
 
@@ -31,81 +31,65 @@ const AdditionalParameterFields = ({
     resetField(`additionnalParameters.${index}.value`);
   };
   return (
-    <>
-      <div className="jp-EodagWidget-additionnalParameters">
-        <div className="jp-EodagWidget-additionnalParameters-label-icon-wrapper">
-          <label className="jp-EodagWidget-section-title">
-            Additional Parameters
-          </label>
-          <a
-            href="https://eodag.readthedocs.io/en/stable/add_provider.html#opensearch-parameters-csv"
-            target="_blank"
-            rel="noopener noreferrer"
-            data-tooltip-id="parameters-information"
-            data-tooltip-content="Click to check queryable metadata in parameters documentation"
-            data-tooltip-variant={tooltipDark}
-            data-tooltip-place={tooltipTop}
-          >
-            <CarbonInformation height="20" width="20" />
-            <Tooltip id="parameters-information" className="jp-Eodag-tooltip" />
-          </a>
-        </div>
+    <div className="jp-EodagWidget-additionnalParameters">
+      <p
+        className="jp-EodagWidget-section-title"
+        style={{ marginBottom: '10px' }}
+      >
+        Custom Parameters
+      </p>
 
-        {!productType ? (
-          <p className="jp-EodagWidget-noParametersMessage">
-            Select a product type to unlock additional parameters.
-          </p>
-        ) : additionalParameters ? (
-          fields.map((field, index) => (
-            <div key={field.id}>
-              <section className={'section'}>
-                <input
-                  placeholder="Name"
-                  {...register(`additionnalParameters.${index}.name` as const)}
-                />
-                <input
-                  placeholder="Value"
-                  {...register(`additionnalParameters.${index}.value` as const)}
-                />
-                <button
-                  type="button"
-                  className="jp-EodagWidget-additionnalParameters-deletebutton"
-                  onClick={() =>
-                    fields.length === 1 ? clearInput(index) : remove(index)
-                  }
-                  data-tooltip-id="parameters-delete"
-                  data-tooltip-content="remove additionnal parameter"
-                  data-tooltip-variant={tooltipWarning}
-                  data-tooltip-place={tooltipTop}
-                >
-                  <CarbonTrashCan height="20" width="20" />
-                  <Tooltip
-                    id="parameters-delete"
-                    className="jp-Eodag-tooltip"
-                  />
-                </button>
-                <button
-                  type="button"
-                  className="jp-EodagWidget-additionnalParameters-addbutton"
-                  onClick={() => append({ name: '', value: '' })}
-                  data-tooltip-id="parameters-add"
-                  data-tooltip-content="add a new additionnal parameter"
-                  data-tooltip-variant={tooltipDark}
-                  data-tooltip-place={tooltipTop}
-                >
-                  <CarbonAddFilled height="20" width="20" />
-                  <Tooltip id="parameters-add" className="jp-Eodag-tooltip" />
-                </button>
-              </section>
-            </div>
-          ))
-        ) : (
-          <p className="jp-EodagWidget-noParametersMessage">
-            Additional parameters are not allowed with this product type.
-          </p>
-        )}
-      </div>
-    </>
+      {!productType ? (
+        <p className="jp-EodagWidget-noParametersMessage">
+          Select a product type to unlock custom parameters.
+        </p>
+      ) : additionalParameters ? (
+        fields.map((field, index) => (
+          <div key={field.id}>
+            <section className={'section'}>
+              <input
+                placeholder="Name"
+                {...register(`additionnalParameters.${index}.name` as const)}
+              />
+              <input
+                placeholder="Value"
+                {...register(`additionnalParameters.${index}.value` as const)}
+              />
+              <button
+                type="button"
+                className="jp-EodagWidget-additionnalParameters-deletebutton"
+                onClick={() =>
+                  fields.length === 1 ? clearInput(index) : remove(index)
+                }
+                data-tooltip-id="parameters-delete"
+                data-tooltip-content="remove custom parameter"
+                data-tooltip-variant={tooltipWarning}
+                data-tooltip-place={tooltipTop}
+              >
+                <CarbonTrashCan height="20" width="20" />
+                <Tooltip id="parameters-delete" className="jp-Eodag-tooltip" />
+              </button>
+              <button
+                type="button"
+                className="jp-EodagWidget-additionnalParameters-addbutton"
+                onClick={() => append({ name: '', value: '' })}
+                data-tooltip-id="parameters-add"
+                data-tooltip-content="add a new custom parameter"
+                data-tooltip-variant={tooltipDark}
+                data-tooltip-place={tooltipTop}
+              >
+                <CarbonAddFilled height="20" width="20" />
+                <Tooltip id="parameters-add" className="jp-Eodag-tooltip" />
+              </button>
+            </section>
+          </div>
+        ))
+      ) : (
+        <p className="jp-EodagWidget-noParametersMessage">
+          Custom parameters are not allowed with this product type.
+        </p>
+      )}
+    </div>
   );
 };
 
