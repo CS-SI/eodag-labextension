2d6d5d4370137490aa37a88f477adcdb111014b7
Sylvain Brunato <sylvain.brunato@c-s.fr>
2025-05-21 15:34:41 +0200

    refactor: computed_field in settings

--- eodag_labextension/config.py
+++ eodag_labextension/config.py
@@ -6,11 +6,9 @@
 
 from __future__ import annotations
 
-import re
 from importlib.metadata import distributions
-from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -20,20 +18,6 @@ class PackageInfo(BaseModel):
     version: str
 
 
-def get_packages_info(filter: Optional[str] = None) -> dict[str, PackageInfo]:
-    """Get packages info"""
-    infos: dict[str, PackageInfo] = {}
-
-    pattern = re.compile(filter) if filter else None
-
-    installed_packages = distributions()
-    for package in installed_packages:
-        package_name = package.metadata["Name"]
-        if pattern is None or (pattern is not None and pattern.fullmatch(package_name)):
-            infos[package_name] = PackageInfo(version=package.version)
-    return infos
-
-
 class Settings(BaseSettings):
     """EODAG Labextension config"""
 
@@ -41,4 +25,14 @@ class Settings(BaseSettings):
 
     debug: bool = False
 
-    packages: dict[str, PackageInfo] = get_packages_info("^eodag.*$")
+    @computed_field
+    def packages(self) -> dict[str, PackageInfo]:
+        """Get packages info"""
+        infos: dict[str, PackageInfo] = {}
+
+        installed_packages = distributions()
+        for package in installed_packages:
+            package_name = package.metadata["Name"]
+            if package_name.startswith("eodag"):
+                infos[package_name] = PackageInfo(version=package.version)
+        return infos
